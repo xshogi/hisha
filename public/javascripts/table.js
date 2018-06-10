f1491c1e18bbbd043eae6e75ae4ea4969e5324d1
Veck Hsiao <fbukevin@gmail.com>
2018-06-10 23:36:16 +0800

    Make cell get into edit mode when double click
    The behavior it's not work as I expected if I use "ondblclick" event to make cell editable then get into edit mode.
    
    By default, the contenteditable attribute is false. After  event was triggered, the attribute was set to true but NOT get into edit mode. User needs to trigger click event again to edit.
    
    As result, I designed the change of contenteditable when first click event is triggered, then the second click event will make the cell editable.

--- public/javascripts/table.js
+++ public/javascripts/table.js
@@ -1,27 +1,27 @@
-// put here temporarily
-	function saveUpdate(object, event) {
-		// press enter/return key equals to key value 13
-	  if (event.which == 13 && event.shiftKey == false) {
-	  	event.preventDefault();	// prevent insertion of a return(new line break)
 
-	    // ajax to save
-	    $.ajax({
-	    	url: '/tables/save',
-	    	method: 'POST',
-	    	data: {
-	    		table_name: table_name,
-	    		row_id: object.dataset.row,	    		
-	    		column: object.dataset.column,
-	    		value: object.dataset.value
-	    	}
-	    })
-	     .done(function(response){
-	     	console.log(response);
-	     })
-	     .fail(function(error, message){
+function saveUpdate(object, event) {
+	// press enter/return key equals to key value 13
+  if (event.which == 13 && event.shiftKey == false) {
+  	event.preventDefault();	// prevent insertion of a return(new line break)
 
-	     })
+    // ajax to save
+    $.ajax({
+    	url: '/tables/save',
+    	method: 'POST',
+    	data: {
+    		table_name: table_name,
+    		row_id: object.dataset.row,	    		
+    		column: object.dataset.column,
+    		value: object.dataset.value
+    	}
+    })
+     .done(function(response){
+     	console.log(response);
+     })
+     .fail(function(error, message){
 
-	    return false;
-	  }
-	}
+     })
+
+    return false;
+  }
+}
